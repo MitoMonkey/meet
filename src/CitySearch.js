d04653a562541b4f3ec10e85b359729474e15e84
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-10-02 11:05:35 +0200

    added: Feat1-Scenario3: select city

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -16,6 +16,11 @@ class CitySearch extends Component {
             suggestions,
         });
     };
+    handleItemClicked = (suggestion) => {
+        this.setState({
+            query: suggestion
+        });
+    }
 
     render() {
         return (
@@ -28,7 +33,10 @@ class CitySearch extends Component {
                 />
                 <ul className="suggestions">
                     {this.state.suggestions.map((suggestion) => (
-                        <li key={suggestion}>{suggestion}</li>
+                        <li 
+                            key={suggestion} 
+                            onClick={() => this.handleItemClicked(suggestion)}
+                        >{suggestion}</li>
                     ))}
                     <li key='all'>
                         <b>See all cities</b>
