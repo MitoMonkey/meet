7184039ce62f49aaeb30f97688ae3dcad930d5fd
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-10-08 11:56:53 +0200

    alert styling improved

--- src/Alert.js
+++ src/Alert.js
@@ -4,18 +4,19 @@ class Alert extends Component {
     constructor(props) {
         super(props);
         this.color = null;
+        this.className = '';
     }
 
     getStyle = () => {
         return {
-            color: this.color,
+            color: this.color            
         };
     }
 
     render() {
         return (
             <div className="Alert">
-                <p style={this.getStyle()}>{this.props.text}</p>
+                <p style={this.getStyle()} className={this.className}>{this.props.text}</p>
             </div>
         );
     }
@@ -25,12 +26,14 @@ class InfoAlert extends Alert {
     constructor(props) {
         super(props);
         this.color = 'blue';
+        this.className = 'InfoAlert';
     }
 }
 class ErrorAlert extends Alert {
     constructor(props) {
         super(props);
         this.color = 'red';
+        this.className = 'ErrorAlert';
     }
 }
 
