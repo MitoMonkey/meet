6ee9eb62763ad4654fdb0db064fa79446b4f50ff
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-10-15 11:06:03 +0200

    indentation fix

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -23,25 +23,25 @@ const EventGenre = ({events}) => {
         setData(getData());
     }, [events]); // listen for changes to the events prop
 
-return (
-    <ResponsiveContainer height={400} >
-        <PieChart width={300} height={400}>
-            <Pie
-                data={data}
-                cx="50%"
-                cy="50%"
-                labelLine={false}
-                // label={renderCustomizedLabel}
-                label={({ name, percent}) => `${name} ${(percent * 100).toFixed(0)}%`}
-                outerRadius={80}
-                fill="#8884d8"
-                dataKey="value"
-            >
-                {// data.map((entry, index) => ( <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} /> ))
-                }
-            </Pie>
-        </PieChart>
-    </ResponsiveContainer>
-)
+    return (
+        <ResponsiveContainer height={400} >
+            <PieChart width={300} height={400}>
+                <Pie
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    labelLine={false}
+                    // label={renderCustomizedLabel}
+                    label={({ name, percent}) => `${name} ${(percent * 100).toFixed(0)}%`}
+                    outerRadius={80}
+                    fill="#8884d8"
+                    dataKey="value"
+                >
+                    {// data.map((entry, index) => ( <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} /> ))
+                    }
+                </Pie>
+            </PieChart>
+        </ResponsiveContainer>
+    )
 }
 export default EventGenre;
